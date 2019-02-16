199606157ef36622db770586e3edeba08337f3ba
hhuwc <1727330498@qq.com>
2019-02-16 22:54:49 +0800

    最近播放组件

---
+++ src/components/home/recent-play.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="recent-play">
+    <h1>最近播放</h1>
+    <scroll class="recent-play-scroll" ref="scroll" :scroll-x="true">
+      <ul ref="songs" class="recent-play-list">
+        <li v-for="n in num" :key="n" class="song-album">
+          <div class="album-pic"></div>
+          <div class="album-title">标题{{n}}</div>
+        </li>
+      </ul>
+    </scroll>
+  </div>
+</template>
+<script>
+import Scroll from "base/scroll";
+export default {
+  components: {
+    Scroll
+  },
+  data() {
+    return {
+      num: 10
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.recent-play {
+  > h1 {
+    font-size: 20px;
+    text-align: center;
+    font-weight: lighter;
+  }
+
+  .recent-play-scroll {
+    margin-top: 12px;
+    .recent-play-list {
+      display: inline-flex;
+      .song-album {
+        margin-left: 20px;
+
+        .album-pic {
+          height: 150px;
+          width: 150px;
+          background-color: #988907;
+        }
+
+        &:last-child {
+          margin-right: 20px;
+        }
+      }
+    }
+  }
+}
+</style>
+
