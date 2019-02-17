393e702e2d0aaf614a2df71edcef38c0af889178
hhuwc <1727330498@qq.com>
2019-02-17 11:41:20 +0800

    scroll 组件

---
+++ src/base/scroll/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div ref="wrapper" class="wc-scroll">
+    <slot></slot>
+  </div>
+</template>
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 2
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    scrollX: {
+      type: Boolean,
+      default: false
+    },
+    scrollY: {
+      type: Boolean,
+      default: true
+    },
+    // 是否监听滚动
+    listenScroll: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initScroll();
+    });
+  },
+  destroyed() {
+    this.$refs.wrapper && this.$refs.wrapper.destroy();
+  },
+  methods: {
+    initScroll() {
+      if (!this.$refs.wrapper) return;
+
+      let options = {
+        probeType: this.probeType,
+        click: this.click,
+        scrollX: this.scrollX,
+        scrollY: this.scrollY
+      };
+
+      this.scroll = new BScroll(this.$refs.wrapper, options);
+      if (this.listenScroll) {
+        this.scroll.on("scroll", pos => {
+          this.$emit("scroll", pos);
+        });
+      }
+    },
+    disable() {
+      this.scroll && this.scroll.disable();
+    },
+    enable() {
+      this.scroll && this.scroll.enable();
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh();
+    }
+  }
+};
+</script>
+
