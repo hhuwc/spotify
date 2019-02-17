77c4f3ec6ea08dcd9a993e606197553cf48e0760
hhuwc <1727330498@qq.com>
2019-02-17 19:09:06 +0800

    app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+
+    <bottom-bar/>
+  </div>
+</template>
+
+<script>
+import BottomBar from "components/bottom-nav";
+export default {
+  components: {
+    BottomBar
+  }
+};
+</script>
+
+<style lang="scss">
+@import "theme/reset.scss";
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
