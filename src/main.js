a1e75add882bbdbcdfc06075c0dafdafb4d592a9
hhuwc <1727330498@qq.com>
2019-02-24 17:37:05 +0800

    lazyload

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue';
 import App from "./App";
 import router from "./router";
 import store from "./store";
+import VueLazyload from 'vue-lazyload';
 
+Vue.use(VueLazyload);
+
+// or with options
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  attempt: 1,
+});
 
 new Vue({
   router,
