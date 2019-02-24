0ea2985eb3548d6f12d944e8ec42b09205daebe8
hhuwc <1727330498@qq.com>
2019-02-24 17:36:56 +0800

    miniplayer 原型

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,20 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
-
+    <h1 class="mini-player" v-if="showMiniBar">mini播放器</h1>
     <bottom-bar/>
   </div>
 </template>
 
 <script>
 import BottomBar from "components/bottom-nav";
+import { mapState } from "vuex";
 export default {
   components: {
     BottomBar
+  },
+  computed: {
+    ...mapState(["showMiniBar"])
   }
 };
 </script>
@@ -26,10 +30,16 @@ body,
   height: 100%;
 }
 #app {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  position: relative;
+
+  .mini-player {
+    color: #fff;
+    background-color: red;
+    height: 50px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 60px;
+  }
 }
 </style>
