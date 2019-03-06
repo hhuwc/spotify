09614cc9f950ac941a56b3fed5fd083c98d8cc94
hhuwc <1727330498@qq.com>
2019-03-06 14:08:29 +0800

    loading

--- src/base/loading/index.vue
+++ src/base/loading/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="loading-wrapper">
-    <div class="loading-circle" :style="style"></div>
+    <i class="spfont sp-loading" :style="style"></i>
   </div>
 </template>
 
@@ -15,8 +15,7 @@ export default {
   computed: {
     style() {
       return {
-        height: this.size + "px",
-        width: this.size + "px"
+        fontSize: this.size + "px"
       };
     }
   }
@@ -25,7 +24,7 @@ export default {
 
 <style lang="scss">
 $--main-green: #1db954;
-$--back-color: #b2d9c0;
+
 $--background-color: #111111;
 @keyframes spin {
   from {
@@ -37,32 +36,13 @@ $--background-color: #111111;
   }
 }
 .loading-wrapper {
-  .loading-circle {
-    background: rgb(29, 185, 84);
-    background: linear-gradient(
-      0deg,
-      rgba(29, 185, 84, 1) 0%,
-      rgba(252, 252, 255, 1) 20%,
-      rgba(252, 252, 255, 1) 100%
-    );
-    position: relative;
-    border-radius: 100%;
-
+  color: $--main-green;
+  background-color: $--background-color;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  > .sp-loading {
     animation: spin 2s infinite linear;
-    margin: 0 auto;
-
-    &::before {
-      height: 80%;
-      width: 80%;
-      display: block;
-      content: "";
-      background-color: $--background-color;
-      border-radius: 100%;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    }
   }
 }
 </style>
